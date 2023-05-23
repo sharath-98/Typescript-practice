d0aaf29ca0e6198a3210b310d1cf40b2608331fd
sharath-98 <sharathchander.p@gmail.com>
2023-05-23 16:39:44 -0500

    Array and object destructuring

--- interfaces.ts
+++ interfaces.ts
@@ -30,9 +30,29 @@ sharath.id = 1;
 console.log("Employees interface: Sharath => ")
 console.log(sharath);
 
+// Object Destructuring
+let {name : soundName} : User = sound;
+
+console.log("Destructuring bash: " + soundName );
 
 export interface Login{
     login():User;
 }
+
+// Array destructuring 
+let [user1, user2, ...restUsers]: User[] = [
+    { name: "Soundarya", age: 23, id: 3 },
+    { name: "Sharath", age: 23,  id: 2  },
+    { name: "Vishal",  age: 23, id: 1  }
+];
+
+console.log(user1);
+console.log("\nREST USERS:");
+console.log(restUsers);
+
+let resFilter = restUsers.filter(user => user.id > 0);
+console.log("\nFILTER: ");
+console.log(resFilter);
+
 
 
